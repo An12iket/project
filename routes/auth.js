333ad1a9e1ea6ae933bd294f1e7b5bc2d5c41310
an12iket <aniketchavan121012@gmail.com>
2025-05-17 21:01:33 +0530

    added route to update user info

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,13 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const user = require("../models/user");
 const secretKey = process.env.JWT_SECRET;
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/signup", async (req, res) => {
   const { username, password, firstname, lastname } = req.body;
-  
-  if(!username || !password|| !firstname || !lastname){ 
-  return res.status(400).json({ message: "All fields are required !" });
+
+  if (!username || !password || !firstname || !lastname) {
+    return res.status(400).json({ message: "All fields are required !" });
   }
 
   try {
@@ -48,8 +49,7 @@ router.post("/signup", async (req, res) => {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
   }
-})
-
+});
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
@@ -61,7 +61,10 @@ router.post("/login", async (req, res) => {
   try {
     const existingUser = await user.findOne({ username });
 
-    if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
+    if (
+      !existingUser ||
+      !(await bcrypt.compare(password, existingUser.password))
+    ) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
@@ -77,12 +80,29 @@ router.post("/login", async (req, res) => {
         id: existingUser._id,
         username: existingUser.username,
         firstname: existingUser.firstname,
-        lastname: existingUser.lastname
-      }
+        lastname: existingUser.lastname,
+      },
     });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { password, firstname, lastname } = req.body;
+
+  if (!password || !firstname || !lastname) {
+    return res.status(400).json({ message: "All the fields are required" });
+  }
+
+  try {
+    await user.updateOne({ _id: req.userId }, req.body);
+
+    res.json({ message: "Updated successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error while proceeding request" });
   }
 });
 
